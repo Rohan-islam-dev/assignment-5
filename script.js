214da9c539a6db69d9d5f1c72805296a35f53242
Rohan-islam-dev <mdrohanislam253@gmail.com>
2025-08-28 19:19:44 +0600

    add  call history  and time

--- script.js
+++ script.js
@@ -1,39 +1,70 @@
 //heart count
-const hearts = document.querySelectorAll('.heart-count');
-const countEl = document.getElementById('counts');
+const hearts = document.querySelectorAll(".heart-count");
+const countEl = document.getElementById("counts");
 
 for (const heart of hearts) {
-  heart.addEventListener('click', function () {
+  heart.addEventListener("click", function () {
     let counts = parseInt(countEl.innerText);
-     counts += 1;
-     countEl.innerText = counts;
+    counts += 1;
+    countEl.innerText = counts;
   });
 }
 
-
 //callig btn
 
-const callButtons = document.querySelectorAll('#btn-call');
-const starRemoveEl = document.getElementById('star-remove');
+const callButtons = document.querySelectorAll("#btn-call");
+const starRemoveEl = document.getElementById("star-remove");
+const callHistoryList = document.querySelector("#History ul");
 
 for (let i = 0; i < callButtons.length; i++) {
-    callButtons[i].addEventListener('click', function () {
-        
-        let credit = parseInt(starRemoveEl.innerText);
+  callButtons[i].addEventListener("click", function () {
+    let credit = parseInt(starRemoveEl.innerText);
 
-        if(credit < 20){
-            alert('terminate the process')
-            return;
-        }
+    if (credit < 20) {
+      alert("আপনার পর্যাপ্ত কয়েন নেই। কল করতে কমপক্ষে ২০ কয়েন লাগবে");
+      return;
+    }
 
-        const serviceName = document.querySelectorAll("#service")[i].innerText;
-        const number = document.querySelectorAll("#num")[i].innerText;
+    const serviceName = document.querySelectorAll("#service")[i].innerText;
+    const number = document.querySelectorAll("#num")[i].innerText;
 
-        alert(`calling: ${serviceName} number: ${number}`);
+    alert(`calling: ${serviceName} number: ${number}`);
 
-        
-        credit -= 20;
-        starRemoveEl.innerText = credit;
-    })
+    credit -= 20;
+    starRemoveEl.innerText = credit;
+
+    const li = document.createElement("li");
+    li.style.backgroundColor = "#FAFAFA";
+    li.style.borderRadius = "12px";
+    li.style.height = "60px";
+    li.style.marginBottom = "20px";
+    li.style.padding = "10px";
+    li.style.display = "flex";
+    li.style.justifyContent = "space-between";
+    li.style.alignItems = "center";
+
+    const leftDiv = document.createElement("div");
+    leftDiv.style.display = "flex";
+    leftDiv.style.flexDirection = "column";
+
+    const serviceNameDiv = document.createElement("div");
+    serviceNameDiv.innerText = serviceName;
+    serviceNameDiv.style.fontSize = '18px';
+    serviceNameDiv.style.color = '#111111';
+  
+    const numberDiv = document.createElement("div");
+    numberDiv.innerText = number;
+
+    leftDiv.appendChild(serviceNameDiv);
+    leftDiv.appendChild(numberDiv);
+
+    const timeDiv = document.createElement("div");
+    timeDiv.innerText = new Date().toLocaleTimeString();
+
+
+    li.appendChild(leftDiv);
+    li.appendChild(timeDiv);
+
+    callHistoryList.appendChild(li);
+  });
 }
-
