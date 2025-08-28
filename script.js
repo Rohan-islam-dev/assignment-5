ad6d3faafcdf54208156c2ec5ebffde79bc0b996
Rohan-islam-dev <mdrohanislam253@gmail.com>
2025-08-29 01:48:26 +0600

    error fix

--- script.js
+++ script.js
@@ -88,13 +88,13 @@ const copyCount = document.getElementById('copy-count');
 for(let i = 0; i<copybutton.length; i++){
   copybutton[i].addEventListener('click',function(){
     
-    
+    const numbers = document.querySelectorAll(".num")[i].innerText;
     
      let copyCounts = parseInt(copyCount.innerText);
      copyCounts += 1;
      copyCount.innerText = copyCounts;
 
-    const numbers = document.querySelectorAll(".num")[i].innerText;
+    
 
     navigator.clipboard.writeText(numbers);
     alert(`নাম্বার কপি হয়েছে: ${numbers}`)
